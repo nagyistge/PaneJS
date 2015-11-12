a711ee6632a8d2b5d7785345caf5f68b050f7aa3
bubkoo <pengxingjian.pxj@alibaba-inc.com>
2015-11-13 00:24:34 +0800

    merge

---
+++ src/enums/shapeNames.js
@@ -0,0 +1,17 @@
+module.exports = {
+    RECTANGLE: 'rectangle',
+    ELLIPSE: 'ellipse',
+    DOUBLE_ELLIPSE: 'doubleEllipse',
+    RHOMBUS: 'rhombus',
+    LINE: 'line',
+    IMAGE: 'image',
+    ARROW: 'arrow',
+    LABEL: 'label',
+    CYLINDER: 'cylinder',
+    SWIMLANE: 'swimlane',
+    CONNECTOR: 'connector',
+    ACTOR: 'actor',
+    CLOUD: 'cloud',
+    TRIANGLE: 'triangle',
+    HEXAGON: 'hexagon'
+};
