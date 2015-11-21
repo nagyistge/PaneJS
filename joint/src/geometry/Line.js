7825fb4790ac3962ad631a54e2bf9383b690ff05
bubkoo <pengxingjian.pxj@alibaba-inc.com>
2015-11-22 00:28:54 +0800

    temp

--- joint/src/geometry/Line.js
+++ joint/src/geometry/Line.js
@@ -1,1 +1,120 @@
+function Line(p1, p2) {
 
+    var that = this;
+
+    if (!(that instanceof Line)) {
+        return new Line(p1, p2);
+    }
+
+    that.start = point(p1);
+    that.end = point(p2);
+}
+
+Line.prototype = {
+
+
+    // @return {double} length of the line
+    length: function () {
+        return sqrt(this.squaredLength());
+    },
+
+    // @return {integer} length without sqrt
+    // @note for applications where the exact length is not necessary (e.g. compare only)
+    squaredLength: function () {
+        var x0 = this.start.x;
+        var y0 = this.start.y;
+        var x1 = this.end.x;
+        var y1 = this.end.y;
+        return (x0 -= x1) * x0 + (y0 -= y1) * y0;
+    },
+
+    midpoint: function () {
+        return point((this.start.x + this.end.x) / 2,
+            (this.start.y + this.end.y) / 2);
+    },
+
+    // @return {point} Point where I'm intersecting l.
+    // @see Squeak Smalltalk, LineSegment>>intersectionWith:
+    intersection: function (l) {
+        var pt1Dir = point(this.end.x - this.start.x, this.end.y - this.start.y);
+        var pt2Dir = point(l.end.x - l.start.x, l.end.y - l.start.y);
+        var det = (pt1Dir.x * pt2Dir.y) - (pt1Dir.y * pt2Dir.x);
+        var deltaPt = point(l.start.x - this.start.x, l.start.y - this.start.y);
+        var alpha = (deltaPt.x * pt2Dir.y) - (deltaPt.y * pt2Dir.x);
+        var beta = (deltaPt.x * pt1Dir.y) - (deltaPt.y * pt1Dir.x);
+
+        if (det === 0 ||
+            alpha * det < 0 ||
+            beta * det < 0) {
+            // No intersection found.
+            return null;
+        }
+        if (det > 0) {
+            if (alpha > det || beta > det) {
+                return null;
+            }
+        } else {
+            if (alpha < det || beta < det) {
+                return null;
+            }
+        }
+        return point(this.start.x + (alpha * pt1Dir.x / det),
+            this.start.y + (alpha * pt1Dir.y / det));
+    },
+
+    // @return the bearing (cardinal direction) of the line. For example N, W, or SE.
+    // @returns {String} One of the following bearings : NE, E, SE, S, SW, W, NW, N.
+    bearing: function () {
+
+        var lat1 = toRad(this.start.y);
+        var lat2 = toRad(this.end.y);
+        var lon1 = this.start.x;
+        var lon2 = this.end.x;
+        var dLon = toRad(lon2 - lon1);
+        var y = sin(dLon) * cos(lat2);
+        var x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+        var brng = toDeg(atan2(y, x));
+
+        var bearings = ['NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'];
+
+        var index = brng - 22.5;
+        if (index < 0) {
+            index += 360;
+        }
+        index = parseInt(index / 45);
+
+        return bearings[index];
+    },
+
+    // @return {point} my point at 't' <0,1>
+    pointAt: function (t) {
+        var x = (1 - t) * this.start.x + t * this.end.x;
+        var y = (1 - t) * this.start.y + t * this.end.y;
+        return point(x, y);
+    },
+
+    // @return {number} the offset of the point `p` from the line.
+    // + if the point `p` is on the right side of the line,
+    // - if on the left and 0 if on the line.
+    pointOffset: function (p) {
+        // Find the sign of the determinant of vectors (start,end), where p is the query point.
+        return ((this.end.x - this.start.x) * (p.y - this.start.y) - (this.end.y - this.start.y) * (p.x - this.start.x)) / 2;
+    },
+
+
+    valueOf: function () {
+        return [this.satrt.x, this.start.y, this.end.x, this.end.y];
+    },
+
+    toString: function () {
+        return this.start.toString() + ' ' + this.end.toString();
+    },
+
+    equals: function (l) {
+        return this.x === l.x && this.y === l.y;
+    },
+
+    clone: function () {
+        return new Line(this);
+    }
+};
--- joint/src/geometry/Point.js
+++ joint/src/geometry/Point.js
@@ -9,12 +9,15 @@ import {
 var math = Math;
 var PI = math.PI;
 var abs = math.abs;
+var cos = math.cos;
+var sin = math.sin;
 var mmin = math.min;
 var mmax = math.max;
 var sqrt = math.sqrt;
 var atan2 = math.atan2;
 var round = math.round;
 var floor = math.floor;
+var random = math.random;
 
 
 function Point(x, y) {
@@ -50,12 +53,42 @@ function Point(x, y) {
 
 Point.prototype = {
 
-    valueOf: function () {
-        return [this.x, this.y];
+    constructor: Point,
+
+    update: function (x, y) {
+
+        var that = this;
+
+        that.x = x || 0;
+        that.y = y || 0;
+
+        return that;
     },
 
-    toString: function () {
-        return this.valueOf().join('@');
+    offset: function (dx, dy) {
+
+        // Offset me by the specified amount.
+
+        var that = this;
+
+        that.x += dx || 0;
+        that.y += dy || 0;
+
+        return that;
+    },
+
+    round: function (precision) {
+
+        var that = this;
+
+        that.x = precision ? toFixed(that.x, precision) : round(that.x);
+        that.y = precision ? toFixed(that.y, precision) : round(that.y);
+
+        return that;
+    },
+
+    difference: function (p) {
+        return new Point(this.x - p.x, this.y - p.y);
     },
 
     adhereToRect: function (rect) {
@@ -88,12 +121,13 @@ Point.prototype = {
         // Makes sure that the comparison with zero takes rounding errors into account.
         var PRECISION = 10;
         // Note that `atan2` is not defined for `x`, `y` both equal zero.
-        var rad = ( toFixed(y, PRECISION) == 0 && toFixed(x, PRECISION) == 0) ? 0 : atan2(y, x);
+        var rad = (toFixed(y, PRECISION) === 0 && toFixed(x, PRECISION) === 0) ? 0 : atan2(y, x);
 
         // Correction for III. and IV. quadrant.
         if (rad < 0) {
             rad = 2 * PI + rad;
         }
+
         return 180 * rad / PI;
     },
 
@@ -113,84 +147,74 @@ Point.prototype = {
         return abs(p.x - this.x) + abs(p.y - this.y);
     },
 
-    offset: function (dx, dy) {
-
-        // Offset me by the specified amount.
-
-        var that = this;
-
-        that.x += dx || 0;
-        that.y += dy || 0;
-
-        return that;
-    },
-
     magnitude: function () {
-        return sqrt((this.x * this.x) + (this.y * this.y)) || 0.01;
-    },
-
-    update: function (x, y) {
-
-        var that = this;
-
-        that.x = x || 0;
-        that.y = y || 0;
-
-        return that;
-    },
-
-    round: function (precision) {
-
-        var that = this;
-
-        that.x = precision ? toFixed(that.x, precision) : round(that.x);
-        that.y = precision ? toFixed(that.y, precision) : round(that.y);
-
-        return that;
+        return sqrt((this.x * this.x) + (this.y * this.y)) || 0.0001;
     },
 
     normalize: function (len) {
+
         // Scale the line segment between (0,0) and me to have a length of len.
-        var s = (len || 1) / this.magnitude();
-        this.x = s * this.x;
-        this.y = s * this.y;
-        return this;
+
+        // 可以通过 x 和 y 的比例来求?
+
+        var that = this;
+        var s = (len || 1) / that.magnitude();
+
+        that.x = s * that.x;
+        that.y = s * that.y;
+
+        return that;
     },
 
-    difference: function (p) {
-        return new Point(this.x - p.x, this.y - p.y);
-    },
 
     // Return the bearing between me and point `p`.
     bearing: function (p) {
         return line(this, p).bearing();
     },
 
-    // Converts rectangular to polar coordinates.
-    // An origin can be specified, otherwise it's 0@0.
+
     toPolar: function (o) {
-        o = (o && point(o)) || point(0, 0);
-        var x = this.x;
-        var y = this.y;
-        this.x = sqrt((x - o.x) * (x - o.x) + (y - o.y) * (y - o.y)); // r
-        this.y = toRad(o.theta(point(x, y)));
-        return this;
+
+        // Converts rectangular to polar coordinates.
+        // An origin can be specified, otherwise it's `0 0`.
+
+
+        o = o && new Point(o) || new Point(0, 0);
+
+        var that = this;
+        var x = that.x;
+        var y = that.y;
+
+        that.x = sqrt((x - o.x) * (x - o.x) + (y - o.y) * (y - o.y)); // r
+        that.y = toRad(o.theta(point(x, y)));
+
+        return that;
     },
 
-    // Rotate point by angle around origin o.
     rotate: function (o, angle) {
+
+        // Rotate point by angle around origin o.
+
         angle = (angle + 360) % 360;
-        this.toPolar(o);
-        this.y += toRad(angle);
-        var p = point.fromPolar(this.x, this.y, o);
-        this.x = p.x;
-        this.y = p.y;
-        return this;
+
+        var that = this;
+
+        that.toPolar(o);
+        that.y += toRad(angle);
+
+        var p = Point.fromPolar(that.x, that.y, o);
+
+        that.x = p.x;
+        that.y = p.y;
+        return that;
     },
 
-    // Move point on line starting from ref ending at me by
-    // distance distance.
+
     move: function (ref, distance) {
+
+        // Move point on line starting from ref
+        // ending at me by distance distance.
+
         var theta = toRad(point(ref).theta(this));
         return this.offset(cos(theta) * distance, -sin(theta) * distance);
     },
@@ -202,9 +226,6 @@ Point.prototype = {
         return point(this).offset(-dx, -dy).theta(ref) - this.theta(ref);
     },
 
-    equals: function (p) {
-        return this.x === p.x && this.y === p.y;
-    },
 
     snapToGrid: function (gx, gy) {
         this.x = snapToGrid(this.x, gx);
@@ -218,9 +239,52 @@ Point.prototype = {
         return point(ref).move(this, this.distance(ref));
     },
 
+    valueOf: function () {
+        return [this.x, this.y];
+    },
+
+    toString: function () {
+        return this.valueOf().join('@');
+    },
+
+    equals: function (p) {
+        return this.x === p.x && this.y === p.y;
+    },
+
     clone: function () {
         return new Point(this);
     }
+};
+
+
+Point.fromPolar = function (r, angle, o) {
+
+    // Alternative constructor, from polar coordinates.
+    // @param {number} r Distance.
+    // @param {number} angle Angle in radians.
+    // @param {point} [optional] o Origin.
+
+    o = (o && point(o)) || point(0, 0);
+    var x = abs(r * cos(angle));
+    var y = abs(r * sin(angle));
+    var deg = normalizeAngle(toDeg(angle));
+
+    if (deg < 90) {
+        y = -y;
+    } else if (deg < 180) {
+        x = -x;
+        y = -y;
+    } else if (deg < 270) {
+        x = -x;
+    }
+
+    return point(o.x + x, o.y + y);
+};
+
+Point.random = function (x1, x2, y1, y2) {
+    // Create a point with random coordinates that fall
+    // into the range `[x1, x2]` and `[y1, y2]`.
+    return new Point(floor(random() * (x2 - x1 + 1) + x1), floor(random() * (y2 - y1 + 1) + y1));
 };
 
 
