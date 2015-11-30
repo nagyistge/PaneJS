5ad8b2b809bd4cd30ee61c1e8ccd6ac3ea92615a
bubkoo <pengxingjian.pxj@alibaba-inc.com>
2015-12-01 01:55:11 +0800

    uuid, hashCode

--- src/utils/string.js
+++ src/utils/string.js
@@ -27,11 +27,44 @@ function trim(str) {
     return str ? proto.trim.call('' + str) : '';
 }
 
+function uuid() {
+
+    // credit: http://stackoverflow.com/posts/2117523/revisions
+
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0;
+        var v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function hashCode(str) {
+
+    // Return a simple hash code from a string.
+    // See http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/.
+
+    var hash = 0;
+    var length = str.length;
+
+    if (length === 0) {
+        return hash;
+    }
+
+    for (var i = 0; i < length; i++) {
+        var c = str.charCodeAt(i);
+        hash = ((hash << 5) - hash) + c;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+
+    return hash;
+}
 
 export {
     lc,
     uc,
     trim,
+    uuid,
+    hashCode,
     toString,
     sanitizeText
 }
