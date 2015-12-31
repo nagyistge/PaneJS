35e95cccd8a40680c6a351f21ec0ae20564aa9ff
bubkoo <pengxingjian.pxj@alibaba-inc.com>
2016-01-01 02:20:58 +0800

    code coverage with istanbul

---
+++ test/spec/utils/array.spec.js
@@ -0,0 +1,9 @@
+let expect = require('chai').expect;
+let array = require('../../src/utils/array');
+
+describe('utils/array', function () {
+
+    it('indexOf(arr, item)', function () {
+        expect(1).to.be.a('number');
+    });
+});
