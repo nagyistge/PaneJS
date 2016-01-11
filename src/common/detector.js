010fbf9abad0f5f397a255bd24a0ff08b0cf0494
bubkoo <bubkoo@163.com>
2016-01-12 02:12:00 +0800

    close#8

--- src/common/detector.js
+++ src/common/detector.js
@@ -6,6 +6,7 @@ let av = navigator.appVersion;
 // -------
 
 export default {
+
     // IE
     IS_IE: ua.indexOf('MSIE') >= 0,
 
@@ -28,20 +29,20 @@ export default {
 
     // True if -o-transform is available as a CSS style. This is the case
     // for Opera browsers that use Presto/2.5 and later.
-    IS_OT: ua.indexOf('Presto/2.4.') < 0 &&
-    ua.indexOf('Presto/2.3.') < 0 &&
-    ua.indexOf('Presto/2.2.') < 0 &&
-    ua.indexOf('Presto/2.1.') < 0 &&
-    ua.indexOf('Presto/2.0.') < 0 &&
-    ua.indexOf('Presto/1.') < 0,
+    IS_OT: ua.indexOf('Presto/2.4.') < 0
+    && ua.indexOf('Presto/2.3.') < 0
+    && ua.indexOf('Presto/2.2.') < 0
+    && ua.indexOf('Presto/2.1.') < 0
+    && ua.indexOf('Presto/2.0.') < 0
+    && ua.indexOf('Presto/1.') < 0,
 
     // True if -moz-transform is available as a CSS style. This is the case
     // for all Firefox-based browsers newer than or equal 3, such as Camino,
     // Iceweasel, Seamonkey and Iceape.
-    IS_MT: (ua.indexOf('Firefox/') >= 0 && ua.indexOf('Firefox/1.') < 0 && ua.indexOf('Firefox/2.') < 0) ||
-    (ua.indexOf('Iceweasel/') >= 0 && ua.indexOf('Iceweasel/1.') < 0 && ua.indexOf('Iceweasel/2.') < 0) ||
-    (ua.indexOf('SeaMonkey/') >= 0 && ua.indexOf('SeaMonkey/1.') < 0) ||
-    (ua.indexOf('Iceape/') >= 0 && ua.indexOf('Iceape/1.') < 0),
+    IS_MT: (ua.indexOf('Firefox/') >= 0 && ua.indexOf('Firefox/1.') < 0 && ua.indexOf('Firefox/2.') < 0)
+    || (ua.indexOf('Iceweasel/') >= 0 && ua.indexOf('Iceweasel/1.') < 0 && ua.indexOf('Iceweasel/2.') < 0)
+    || (ua.indexOf('SeaMonkey/') >= 0 && ua.indexOf('SeaMonkey/1.') < 0)
+    || (ua.indexOf('Iceape/') >= 0 && ua.indexOf('Iceape/1.') < 0),
 
     IS_IOS: !!ua.match(/(iPad|iPhone|iPod)/g),
 
