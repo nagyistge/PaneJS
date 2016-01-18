56544f49430887b0b6455c64535dd8c2fffc4c88
leungwensen <leungwensen@gmail.com>
2016-01-18 14:18:24 +0800

    adding life-cycle methods for Handler

--- src/handlers/Handler.js
+++ src/handlers/Handler.js
@@ -1,6 +1,45 @@
 // import * as utils from '../common/utils';
+import Events from '../common/Events';
 
-class Handler {
+class Handler extends Events {
+    constructor(paper, options = {}) {
+        super();
+
+        let that = this;
+
+        that.paper = paper;
+        that.disabled = false;
+
+        that.init(options)
+            .on('handler:initalized');
+    }
+
+    // life cycle
+    // ----------
+    init() {
+        return this;
+    }
+
+    destroy() {
+    }
+
+    disable() {
+        let that = this;
+
+        that.disabled = true;
+        that.on('handler:disabled');
+
+        return that;
+    }
+
+    enable() {
+        let that = this;
+
+        that.disabled = false;
+        that.on('handler:enabled');
+
+        return that;
+    }
 }
 
 // exports
