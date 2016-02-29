a035653edf5bc16e44695fd31afceeae9f08e089
leungwensen <leungwensen@gmail.com>
2016-02-29 17:19:18 +0800

    Decorator

--- src/decorators/Decorator.js
+++ src/decorators/Decorator.js
@@ -1,1 +1,30 @@
+// import * as utils from '../common/utils';
+import Events from '../common/Events';
 
+class Decorator extends Events {
+    constructor(paper, options = {}) {
+        super();
+
+        let that = this;
+
+        that.paper = paper;
+        that.model = paper.model;
+
+        that.init(options);
+        that.on('decorator:initalized');
+    }
+
+    // life cycle
+    // ----------
+    init() {
+        return this;
+    }
+
+    destroy() {
+    }
+}
+
+// exports
+// -------
+
+export default Decorator;
