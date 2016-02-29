65b3a9a760529425a51e08e73b90cb953f1c5ae2
leungwensen <leungwensen@gmail.com>
2016-02-29 17:18:29 +0800

    simplify scripts/server.js

--- scripts/server.js
+++ scripts/server.js
@@ -9,7 +9,7 @@ var WebpackDevServer = require('webpack-dev-server');
 
 
 // server
-var port = 9090;
+var port = 7070;
 var host = '127.0.0.1';
 var wwwRoot = path.resolve(__dirname, '../');
 
@@ -20,51 +20,33 @@ var entry = config.entry[pkg.name];
 delete config.entry[pkg.name + '-' + pkg.version];
 
 // config the dev server
-config.entry[pkg.name] = [
-  'webpack-dev-server/client?http://' + host + ':' + port,
-  'webpack/hot/dev-server',
-  entry
-];
+//config.entry[pkg.name] = [
+//  'webpack-dev-server/client?http://' + host + ':' + port,
+//  'webpack/hot/dev-server',
+//  entry
+//];
 
 
-config.plugins.push(
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin()
-);
+//config.plugins.push(
+//  new webpack.HotModuleReplacementPlugin(),
+//  new webpack.NoErrorsPlugin()
+//);
 
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
   contentBase: wwwRoot,
   publicPath: config.output.publicPath,
-  hot: true,
-  noInfo: false,
-  historyApiFallback: true,
+  //hot: true,
+  //noInfo: false,
+  //historyApiFallback: true,
   stats: {
+    cached: false,
+    exclude: [
+      /node_modules[\\\/]/
+    ],
     colors: true
   }
 });
 
 
 server.listen(port, host);
-
-server.listeningApp
-  .on('listening', function () {
-
-    var target = 'http://' + host + ':' + port;
-
-    console.log('Dev server started on: ' + target);
-
-    open(target + '/demo/index.html');
-
-  })
-  .on('error', function (err) {
-
-    if (err.code === 'EADDRINUSE') {
-      console.log('Port ' + port + ' is already in use by another process.');
-    } else {
-      console.log(err);
-    }
-
-    process.exit(err.code);
-
-  });
